99125d241cd2bc89afad840fce3a830945753682
mh <mh@immerda.ch>
2008-10-06 23:22:40 +0000

    better naming so no confusion can happen

--- manifests/defines.pp
+++ manifests/defines.pp
@@ -3,7 +3,7 @@
 define openvpn::add_client_infos(
     $client_dir = '/etc/openvpn/clients',
     $client_tool = 'ifconfig-push',
-    $ipaddress,
+    $client_ipaddress,
     $subnet = '255.255.255.0',
     $default_gw = 'absent'
 ){
@@ -12,7 +12,7 @@ define openvpn::add_client_infos(
         default: { $gw_string = $default_gw }
     }
    file{"${client_dir}/${name}":
-        content => "${client_too} ${ipaddress} ${subnet} ${gw_string}",
+        content => "${client_too} ${client_ipaddress} ${subnet} ${gw_string}",
         require => File['/etc/openvpn/clients'],
         owner => root, group => 0, mode => 0644;
     } 
