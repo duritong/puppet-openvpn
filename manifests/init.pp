b5a1bcf518d8f1eeb3c8cd5126fe3a5a67e56fa1
mh <mh@immerda.ch>
2008-10-07 18:46:33 +0000

    update openbsd call

--- manifests/init.pp
+++ manifests/init.pp
@@ -45,7 +45,7 @@ class openvpn::openbsd inherits openvpn::base {
     Service[openvpn]{
         hasrestart => false,
         hasstatus => false,
-        start => '/usr/local/sbin/openvpn /etc/openvpn/server.conf',
+        start => 'cd /etc/openvpn/; /usr/local/sbin/openvpn --daemon --config /etc/openvpn/server.conf',
         restart => "kill -HUP `ps ax | grep /usr/local/sbin/openvpn | grep -v grep | awk '{print $1}'`",
         stop => "kill `ps ax | grep /usr/local/sbin/openvpn | grep -v grep | awk '{print $1}'`",
     }
