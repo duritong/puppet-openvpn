a7d06f913f9dbd59e41b9ed5376072739af930cc
o <o@immerda.ch>
2013-12-19 18:24:36 +0100

    deploy the keys and certs

--- manifests/base.pp
+++ manifests/base.pp
@@ -11,16 +11,20 @@ class openvpn::base {
     hasrestart => true,
   }
 
-  file{'/etc/openvpn/server.conf':
-    source => [ "puppet:///modules/site_openvpn/${::fqdn}/server.conf",
-                "puppet:///modules/site_openvpn/server.conf",
-                "puppet:///modules/openvpn/server.conf" ],
-    notify => Service[openvpn],
-    owner => root, group => 0, mode => 0600;
-  }
-
-  file{'/etc/openvpn/clients':
-    ensure => directory,
-    owner => root, group => 0, mode => 0755;
+  file{
+    '/etc/openvpn/server.conf':
+      source  => ["puppet:///modules/site_openvpn/${::fqdn}/server.conf",
+                  "puppet:///modules/site_openvpn/server.conf",
+                  "puppet:///modules/openvpn/server.conf"],
+      notify  => Service[openvpn],
+      owner   => root,
+      group   => 0,
+      mode    => 0600;
+    '/etc/openvpn/clients':
+      ensure  => directory,
+      owner   => 'root',
+      group   => 0,
+      mode    => 0755,
+      require => Service[openvpn];
   }
 }
---
+++ manifests/instance.pp
@@ -0,0 +1,29 @@
+class openvpn::instance (
+  $ensure = 'present',
+  $key_size = '4096',
+) {
+  if $ensure == 'present' {
+    include openvpn
+
+    exec{
+      'build-dh':
+        cwd      => '/etc/openvpn',
+        command  => "/usr/bin/openssl dhparam -out dh.pem ${KEY_SIZE}",
+        creates  => "/etc/openvpn/dh.pem",
+        require  => Service['openvpn'];
+    }
+
+    file{
+      '/etc/openvpn/ca.crt':
+        source  => ["puppet:///modules/site_openvpn/${::fqdn}/ca.crt",
+                    "puppet:///modules/site_openvpn/ca.crt"],
+        require => Service['openvpn'];
+      '/etc/openvpn/server.crt':
+        source  => ["puppet:///modules/site_openvpn/${::fqdn}/server.crt"],
+        require => Service['openvpn'];
+      '/etc/openvpn/server.key':
+        source  => ["puppet:///modules/site_openvpn/${::fqdn}/server.key"],
+        require => Service['openvpn'];
+    }
+  }
+}
