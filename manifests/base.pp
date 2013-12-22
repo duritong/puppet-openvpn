a1587f658cfe7394509c89075e303c7b03f535f1
o <o@immerda.ch>
2013-12-22 14:41:17 +0100

    generate the csr on the server itself

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,4 +1,14 @@
-class openvpn::base {
+class openvpn::base (
+  $key_size = 4096,
+  $key_expire = 365,
+  $key_country,
+  $key_state,
+  $key_locality,
+  $key_organization,
+  $key_organization_unit,
+  $key_common_name,
+  $key_email
+) {
   package{'openvpn':
     ensure => installed,
   }
@@ -12,35 +22,50 @@ class openvpn::base {
   }
 
   file{
+    '/etc/openvpn':
+      ensure  => directory,
+      require => Package[openvpn];
     '/etc/openvpn/server.conf':
       source  => ["puppet:///modules/site_openvpn/${::fqdn}/server.conf",
                   "puppet:///modules/site_openvpn/server.conf",
                   "puppet:///modules/openvpn/server.conf"],
-      notify  => Service[openvpn],
       owner   => root,
       group   => 0,
-      mode    => 0600;
+      mode    => 0600,
+      notify  => Service[openvpn],
+      require => File['/etc/openvpn'];
     '/etc/openvpn/clients':
       ensure  => directory,
       owner   => 'root',
       group   => 0,
       mode    => 0755,
-      require => Service[openvpn];
+      notify  => Service[openvpn],
+      require => File['/etc/openvpn'];
+    '/etc/openvpn/req-config':
+      content => template('openvpn/req-config.erb'),
+      require => File['/etc/openvpn'];
+  }
+
+  exec{
+    'generate-csr':
+      command  => "openssl req -days $key_expire -nodes -new \
+-config /etc/openvpn/req-config -newkey rsa:$key_size \
+-keyout '/etc/openvpn/server.key' -out '/etc/openvpn/server.csr'",
+      creates  => "/etc/openvpn/server.key",
+      requires => File['/etc/openvpn/req-config'];
   }
 
   file{
+    #create those ahead of time with: "openssl dhparam -out dh.pem $key_size"
+    '/etc/openvpn/dh.pem':
+      source  => ["puppet:///modules/site_openvpn/${::fqdn}/dh.pem"],
+      require => File['/etc/openvpn'];
     '/etc/openvpn/ca.crt':
       source  => ["puppet:///modules/site_openvpn/${::fqdn}/ca.crt",
                   "puppet:///modules/site_openvpn/ca.crt"],
-      require => Package[openvpn];
-    '/etc/openvpn/dh.pem':
-      source  => ["puppet:///modules/site_openvpn/${::fqdn}/dh.pem"],
-      require => Package[openvpn];
+      require => File['/etc/openvpn'];
     '/etc/openvpn/server.crt':
       source  => ["puppet:///modules/site_openvpn/${::fqdn}/server.crt"],
-      require => Package[openvpn];
-    '/etc/openvpn/server.key':
-      source  => ["puppet:///modules/site_openvpn/${::fqdn}/server.key"],
-      require => Package[openvpn];
+      require => Exec['generate-csr'];
   }
 }
