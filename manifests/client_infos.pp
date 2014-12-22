6abca68356d3586b57f87702ad45d7a41a8271b3
mh <mh@immerda.ch>
2014-12-22 11:32:23 +0100

    make it ensurable

--- manifests/client_infos.pp
+++ manifests/client_infos.pp
@@ -1,10 +1,11 @@
 # set client specific infos
 define openvpn::client_infos(
+  $zone,
+  $ensure       = 'present',
   $client_dir   = '/etc/openvpn/clients',
   $client_tool  = 'ifconfig-push',
   $client_ip    = undef,
   $server_ip    = undef,
-  $zone
 ) {
   $zone_conf = $openvpn::zones[$zone]
   $iroute = $zone_conf['route']
@@ -23,11 +24,19 @@ define openvpn::client_infos(
 
   file {
     "${client_dir}/${name}":
+      notify  => Service[openvpn],
+      require => File['/etc/openvpn'];
+  }
+  if $ensure == 'present' {
+    File["${client_dir}/${name}"]{
       content => template('openvpn/custom-client.conf.erb'),
       owner   => root,
       group   => 0,
       mode    => '0644',
-      notify  => Service[openvpn],
-      require => File['/etc/openvpn'];
+    }
+  } else {
+    File["${client_dir}/${name}"]{
+      ensure => 'absent',
+    }
   }
 }
