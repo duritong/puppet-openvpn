7f49164d636bda06dea924eb2962f2ba1c1c78bf
mh <mh@immerda.ch>
2017-02-17 17:43:30 +0100

    rearrange class

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,9 +3,6 @@
 # GPLv3
 
 class openvpn (
-  $manage_munin = false,
-  $key_size = 4096,
-  $key_expire = 365,
   $key_country,
   $key_state,
   $key_locality,
@@ -13,32 +10,36 @@ class openvpn (
   $key_organization_unit,
   $key_common_name,
   $key_email,
-  $bind_address = undef,
-  $bind_port = 1194,
-  $protocol = 'udp',
-  $device_type = 'tun',
-  $local_net = ['10.8.0.0', '24'],
-  $push_route = [],
-  $push_default_gw = false,
-  $dns_server = [],
-  $dns_domain = [],
-  $comp_lzo = false,
-  $max_clients = 100,
-  $additional_config = '',
-  $zones = {},
-  $crl = false,
-  $group_name = "",
+  $manage_munin           = false,
+  $key_size               = 4096,
+  $key_expire             = 365,
+  $bind_address           = undef,
+  $bind_port              = 1194,
+  $protocol               = 'udp',
+  $device_type            = 'tun',
+  $local_net              = ['10.8.0.0', '24'],
+  $push_route             = [],
+  $push_default_gw        = false,
+  $dns_server             = [],
+  $dns_domain             = [],
+  $comp_lzo               = false,
+  $max_clients            = 100,
+  $additional_config      = '',
+  $zones                  = {},
+  $crl                    = false,
+  $group_name             = '',
   # This is tls1.2. See --show-tls for more options.
-  $tls_cipher = "TLS-DHE-RSA-WITH-AES-256-GCM-SHA384",
-  $cipher = "AES-128-CBC",
+  $tls_cipher             = 'TLS-DHE-RSA-WITH-AES-256-GCM-SHA384',
+  $cipher                 = 'AES-128-CBC',
+  $masq_interface         = undef,
 ) {
   $local_netmask = cidr2netmask($local_net[1])
 
   case $::operatingsystem {
-    openbsd: { include openvpn::openbsd }
-    default: { include openvpn::base }
+    openbsd: { include ::openvpn::openbsd }
+    default: { include ::openvpn::base }
   }
   if $openvpn::manage_munin {
-    include openvpn::munin
+    include ::openvpn::munin
   }
 }
